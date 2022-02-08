23a3f8b22548942516c554660f58e07474e3dfca
André <ruland@outlook.de>
2022-02-08 14:22:32 +0100

    Bestes UI

---
+++ ppedv.BooksManager/ppedv.BooksManager.UI.TestConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ppedv.BooksManager.Data.Ef;
+using ppedv.BooksManager.Logic;
+
+Console.WriteLine("*** BooksManager v0.1 PREMIUM EDITION ***");
+
+
+var bms = new BooksManagerService(new EfRepository());
+
+var verlag = bms.GetPublisherWithMostExpensivesBooks();
+Console.WriteLine($"Der Verlag {verlag?.Name} hat in Summe die teuersten Bücher");
