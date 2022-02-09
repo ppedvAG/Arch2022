2a560eecdae440c8de0fcabafa221b604f379ca0
André <ruland@outlook.de>
2022-02-09 13:38:12 +0100

    WPF UI mit MVVM

---
+++ ppedv.BooksManager/ppedv.BooksManager.UI.WPF/ViewModels/BooksViewModel.cs
@@ -0,0 +1,60 @@
+﻿using ppedv.BooksManager.Logic;
+using ppedv.BooksManager.Model;
+using System;
+using System.Collections.Generic;
+using System.Windows.Input;
+
+namespace ppedv.BooksManager.UI.WPF.ViewModels
+{
+
+    public class BooksViewModel : ViewModelBase
+    {
+        BooksManagerService _bms = new BooksManagerService(new Data.Ef.EfUnitOfWork());
+
+        public List<Book> Books { get; set; }
+
+        public Book? SelectedBook { get; set; }
+
+        public ICommand SaveCommand { get; set; }
+
+        public DateTime PublishedDate
+        {
+            get => SelectedBook != null ? SelectedBook.Published : DateTime.MinValue;
+            set
+            {
+                if (SelectedBook != null)
+                    SelectedBook.Published = value;
+            }
+        }
+
+        //private Book? selectedBook;
+        //public Book? SelectedBook
+        //{
+        //    get => selectedBook; set
+        //    {
+        //        selectedBook = value;
+        //        OnThisPropertyChanged();
+        //        OnPropertyChanged(nameof(PublishedYearInfo));
+        //    }
+        //}
+
+        public string PublishedYearInfo
+        {
+            get
+            {
+                if (SelectedBook == null)
+                    return "---";
+                else
+                    return $"Vor {DateTime.Now.Year - PublishedDate.Year} Jahren";
+            }
+        }
+
+        public BooksViewModel()
+        {
+            Books = new List<Book>(_bms.UnitOfWork.BooksRepository.Query());
+
+            SaveCommand = new RelayCommand(o => _bms.UnitOfWork.SaveAll());
+        }
+
+    }
+}
---
+++ ppedv.BooksManager/ppedv.BooksManager.UI.WPF/ViewModels/ViewModelBase.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ppedv.BooksManager.UI.WPF.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string proName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(proName));
+        }
+
+        protected void OnThisPropertyChanged([CallerMemberName] string propName = "")
+        {
+            if (!string.IsNullOrWhiteSpace(propName))
+                OnPropertyChanged(propName);
+        }
+    }
+}
