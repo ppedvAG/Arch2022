d50cca230acd284ada092c786f0e8398063253ac
André <ruland@outlook.de>
2022-02-09 16:42:01 +0100

    gRPC spass

---
+++ GrpcService1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcService1;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("http://127.0.0.1:5166",new GrpcChannelOptions() { });
+
+var client = new Greeter.GreeterClient(channel);
+
+
+var res = client.SayHello(new HelloRequest() { Name = "Fred" });
+Console.WriteLine(res.Message);
